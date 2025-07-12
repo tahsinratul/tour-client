25170f150781f4d221d4510a49a0e43d8127fb62
tahsinratul <ratulprofessional123@gmail.com>
2025-07-12 22:44:32 +0600

    doneeee

--- tour/src/main.jsx
+++ tour/src/main.jsx
@@ -4,9 +4,11 @@ import './index.css'
 import router from './router/router.jsx'
 import { RouterProvider } from 'react-router'
 import AuthProvider from './Context/AuthProvider.jsx'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
      <AuthProvider><RouterProvider router={router} /></AuthProvider>
+     <ToastContainer></ToastContainer>
   </StrictMode>,
 )
